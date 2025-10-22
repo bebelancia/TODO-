664b3eee416c5b2f629e08309dca52b2f16c94ae
Isa <isabelabuuu@gmail.com>
2025-10-22 09:41:20 -0300

    Implement task management with add and update functions
    Refactor task addition and management functions.

--- projetoapp/public/js/app.js
+++ projetoapp/public/js/app.js
@@ -2,12 +2,13 @@ let add = document.getElementById("add")
 let tarefa = document.getElementById("tarefa")
 let listaTarefas = document.getElementById("tarefas")
 
+let tarefas = []
+
+
 add.addEventListener("click", () => {
     if (tarefa.value != "") {
-        let li = document.createElement("li")
-        li.textContent = tarefa.value
 
-        listaTarefas.appendChild(li)
+        adicionarTarefa(tarefa.value)
 
         tarefa.value =""
 
@@ -15,3 +16,33 @@ add.addEventListener("click", () => {
         alert("Você deve digitar uma tarefa!")
     }
 })
+
+function adicionarTarefa(descricao) {
+    let novaTarefa = {
+        descricao: descricao,
+        resolvido: false
+    }
+
+    tarefas.push(novaTarefa)
+    
+    atualizarListaTarefas()
+
+}
+
+function atualizarListaTarefas() {
+    limparListaTarefas
+    tarefas.forEach(t => {
+        let li = document.createElement("li")
+        let label = document.createElement("label")
+
+        label.textContent = t.descricao
+        li.appendChild(label)
+
+        listaTarefas.appendChild(li)
+        
+    })
+}
+
+function limparListaTarefas () {
+    listaTarefas.innerHTML =""
+}
